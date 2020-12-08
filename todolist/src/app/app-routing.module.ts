06d17eb4a2972ab5863c319152eccc0e41dd1817
Libert josic <libert.josic@gmail.com>
2020-12-08 23:49:44 +0100

    *main: sorry

--- todolist/src/app/app-routing.module.ts
+++ todolist/src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'todolist',
+    loadChildren: () => import('./todolist/todolist.module').then( m => m.TodolistPageModule)
   }
 ];
 
---
+++ todolist/src/app/todolist/todolist.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.page.html',
+  styleUrls: ['./todolist.page.scss'],
+})
+export class TodolistPage implements OnInit {
+
+  constructor(public alertCtrl: AlertController) { }
+
+  ngOnInit() {
+  }
+
+  async showPrompt() {
+    const prompt = await this.alertCtrl.create({
+      inputs: [
+        {
+          name: 'taskName',
+          type: 'text',
+          placeholder: 'Indiquez votre tâche',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Enregistrer',
+          handler: data => {
+          this.taskName = data.taskName;
+          this.taskList.push(this.taskName);
+          console.log(this.taskList);
+          }
+        },
+        {
+          text: 'Annuler',
+        }
+      ]
+    });
+    await prompt.present();
+  }
+
+  addTask() {
+    if (this.taskName.length > 0) {
+      let task = this.taskName;
+      this.taskList.push(task);
+      this.taskName = '';
+    }
+  }
+  deleteTask(index) {
+    this.taskList.splice(index, 1);
+  }
+}
+
